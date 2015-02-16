9f8fd6ae6ab38320f493a6e07675f2d62b682fb0
tarrsalah <tarrsalah@gmail.com>
2015-02-16 22:45:01 +0100

    get a sample graph an vis it.

--- prototype/go-vis/main.go
+++ prototype/go-vis/main.go
@@ -1,10 +1,44 @@
 package main
 
 import (
+	"encoding/json"
 	"net/http"
 )
 
+type Node struct {
+	Id    int64  `json:"id"`
+	Label string `json:"label"`
+}
+
+type Edge struct {
+	From int64 `json:"from"`
+	To   int64 `json:"to"`
+}
+
+type Graph struct {
+	Nodes []Node `json:"nodes"`
+	Edges []Edge `json:"edges"`
+}
+
 func main() {
+	graph := Graph{
+		Nodes: []Node{
+			Node{1, "one"},
+			Node{2, "two"},
+			Node{3, "three"},
+			Node{4, "four"},
+			Node{5, "five"},
+		},
+		Edges: []Edge{
+			Edge{1, 2},
+			Edge{1, 4},
+			Edge{1, 5},
+			Edge{2, 4},
+			Edge{3, 5},
+			Edge{2, 5},
+		},
+	}
+
 	fs := http.FileServer(http.Dir("./public/"))
 	http.Handle("/public/", http.StripPrefix("/public/", fs))
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
@@ -16,7 +50,11 @@ func main() {
 	})
 
 	http.HandleFunc("/graph", func(w http.ResponseWriter, r *http.Request) {
-		w.Write([]byte("hello graph"))
+		graph_json, err := json.MarshalIndent(&graph, "", " ")
+		if err != nil {
+			http.Error(w, "Can't marshal the graph", http.StatusInternalServerError)
+		}
+		w.Write(graph_json)
 	})
 
 	http.ListenAndServe(":3000", nil)
--- prototype/go-vis/public/js/app.js
+++ prototype/go-vis/public/js/app.js
@@ -1,1 +1,59 @@
-var hello="hello";
+(function(document, vis, m) {
+    var app = {};
+
+    var Node = function(data) {
+	this.id = data.id;
+	this.label = data.label;
+    };
+
+    var Edge = function(data) {
+	this.from = data.from;
+	this.to = data.to;
+    };
+
+    var Graph = function(data) {
+	this.nodes = m.prop(data.nodes);
+	this.edges = m.prop(data.edges);
+    };
+
+    Graph.prototype.get = function() {
+	return m.request({
+	    method:"GET",
+	    url:"/graph",
+	    type:Graph
+	});
+    };
+
+    app.vm = {};
+    app.vm.init = function() {
+	var graph = new Graph({});
+	app.vm.graph = graph.get().then(graph);
+    };
+
+    app.controller = function() {
+	app.vm.init();
+    };
+
+    function draw(element, isInitialized) {
+	if (!isInitialized) {
+	    var data = {
+		nodes: app.vm.graph().nodes(),
+		edges: app.vm.graph().edges()
+	    };
+	     var options = {
+		 width: '800px',
+		 height: '400px'
+	     };
+	    new vis.Network(element, data, options);
+	}
+    }
+
+    app.view = function(controller) {
+	return m("div", {
+	    id: "graph",
+	    config: draw
+	});
+    };
+    m.module(document.getElementById("app"), app);
+    window.app = app;
+}(document, vis, m));
